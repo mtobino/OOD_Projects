c9195506ec5ee6b61c92a41b0caf24f6bf4349fc
Matthew Tobino <tobino68@students.rowan.edu>
2024-01-22 15:45:12 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Extra Credit/Project 0
+ *
+ * @author Matthew Tobino ~ 1/17/24
+ */
+public class Main {
+    public static void main(String[] args) {
+        Simulator sim = new Simulator();
+        sim.runLongSimulation();
+    }
+}
